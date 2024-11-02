8d4abda7b303097bfdfd53f1683a348fee69727c
simone-gasparini-94 <simone.gasparini.2@gmail.com>
2024-11-02 14:26:54 +0100

    added function for rotating the star icon on scroll

--- script.js
+++ script.js
@@ -2,8 +2,18 @@ function updateTime() {
     const options = { hour: '2-digit', minute: '2-digit', second: '2-digit', hour12: false, timeZone: 'Europe/Berlin' };
     const localTime = new Intl.DateTimeFormat('de-DE', options).format(new Date());
     const timeDisplay = document.querySelector("#time-display");
-    timeDisplay.textContent = `${localTime} [DE]`;
+    timeDisplay.textContent = `${localTime}`;
 }
 
 setInterval(updateTime, 1000);
 updateTime();
+
+const star = document.querySelector("#star");
+
+function rotateStar() {
+    const scrollPosition = window.scrollY;
+    const rotationDegrees =  scrollPosition * 0.1;
+    star.style.transform = `rotate(${rotationDegrees}deg)`;
+}
+
+window.addEventListener("scroll", rotateStar);

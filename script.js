cf83631bfc38e385120ce408408794098ba9c2dd
simone-gasparini-94 <simone.gasparini.2@gmail.com>
2024-11-04 16:50:47 +0100

    changed some elements from IDs to classes

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 function updateTime() {
     const options = { hour: '2-digit', minute: '2-digit', second: '2-digit', hour12: false, timeZone: 'Europe/Berlin' };
     const localTime = new Intl.DateTimeFormat('de-DE', options).format(new Date());
-    const timeDisplay = document.querySelector("#time-display");
+    const timeDisplay = document.querySelector(".time-display");
     timeDisplay.textContent = `${localTime}`;
 }
 
 setInterval(updateTime, 1000);
 updateTime();
 
-const star = document.querySelector("#star");
+const star = document.querySelector(".star");
 
 function rotateStar() {
     const scrollPosition = window.scrollY;
